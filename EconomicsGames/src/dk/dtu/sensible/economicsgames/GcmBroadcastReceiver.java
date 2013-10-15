0369db7a0900fa17e7a173034c2959a46c0dd81c
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2013-10-15 11:01:25 +0200

    Initialized repository without secret codes :)

---
+++ EconomicsGames/src/dk/dtu/sensible/economicsgames/GcmBroadcastReceiver.java
@@ -0,0 +1,84 @@
+package dk.dtu.sensible.economicsgames;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: piotr
+ * Date: 7/18/13
+ * Time: 4:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import dk.dtu.sensible.economicsgames.R;
+
+/**
+ * Handling of GCM messages.
+ */
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+    static final String TAG = "AUTH_AuthActivity_broadcastReceiver";
+    public static final String EVENT_MSG_RECEIVED = "EVENT_MSG_RECEIVED";
+    
+    public static final int NOTIFICATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder builder;
+    Context ctx;
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Received: " + intent.getExtras().toString());
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        ctx = context;
+        String messageType = gcm.getMessageType(intent);
+        if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+            sendNotification("Send error: " + intent.getExtras().toString(), AuthActivity.class);
+        } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+            sendNotification("Deleted messages on server: " +
+                    intent.getExtras().toString(), AuthActivity.class);
+        } else {
+        	Intent notifyIntent = new Intent(EVENT_MSG_RECEIVED);
+        	notifyIntent.putExtras(intent.getExtras());
+        	LocalBroadcastManager.getInstance(context).sendBroadcast(notifyIntent);
+
+        	// TODO: Change classes based on the extras in the gcm message?
+        	String msgType = intent.getExtras().getString("type");
+            Log.d(TAG, "msg-type: " + msgType);
+        	if (msgType.equalsIgnoreCase("economics-pgg-init")) {
+        		sendNotification(intent.getExtras().getString("title"), FullscreenActivity.class);
+        	} else {
+	            sendNotification(intent.getExtras().getString("title"), AuthActivity.class);
+        	}
+        }
+        setResultCode(Activity.RESULT_OK);
+    }
+
+    // Put the GCM message into a notification and post it.
+    private void sendNotification(String msg, Class activity) {
+        mNotificationManager = (NotificationManager)
+                ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
+                new Intent(ctx, activity), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ctx)
+                        .setSmallIcon(R.drawable.red_logo5)
+                        .setContentTitle("GCM Notification")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+}
---
+++ gcm-server/src/com/google/android/gcm/server/Main.java
@@ -0,0 +1,15 @@
+
+package com.google.android.gcm.server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Sender sender = new Sender("AIzaSyDt66UAtAQ4DowgMSJ3ZaH3-XI1VaF7msk");
+        Message.Builder b = new Message.Builder();
+        b.addData("type", "economics-pgg-init");
+        try {
+            sender.send(b.build(), "APA91bEE22wgdY_DWNC2mG9Y3IYwxSLCmb4hs2TZNxVGd1mEwd_76BxWCs_Uf3OFqM7M254CHjFG6r2x0MLHLEnoArVHsj-XlbPyiLEHrhB9jABRpAoV8QXHjwPP4MXD0HpGYk6mjY_3gQR0TAlpOl9FSRPtlWF91xvPBoVltkjj2ZNvYJbYdVk", 1);
+        } catch (IOException e) {}
+    }
+}
